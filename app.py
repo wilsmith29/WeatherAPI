4efe96fbfac09764a5f5c62320e09035a84c34bb
William Smith <wilsmith@pdx.edu>
2025-12-14 16:19:21 -0800

    Added functionality to get weather from anywhere

--- app.py
+++ app.py
@@ -1,12 +1,38 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 #from bs4 import BeautifulSoup
 import requests
 app = Flask(__name__)
 
 @app.route('/')
+def home():
+    return jsonify({"home": "weather API running"})
+
+@app.route('/forecast')
 def fetch_weather():
-    url = 'https://api.weather.gov/gridpoints/PQR/113,104/forecast'
-    data = requests.get(url).json()
+    lat = request.args.get("lat")
+    lon = request.args.get("lon")
+
+    if not lat or not lon:
+        return jsonify({"Error": "lat and lon are required"}), 400
+    
+    points_url = f"https://api.weather.gov/points/{lat}, {lon}"
+    points_resp = requests.get(points_url)
+
+    if points_resp.status_code != 200:
+        return jsonify({"error": "Invalid location"}), 400
+    
+    points_data = points_resp.json()
+
+    grid_id = points_data["properties"]["gridId"]
+    grid_x = points_data["properties"]["gridX"]
+    grid_y = points_data["properties"]["gridY"]
+
+    forecast_url = f'https://api.weather.gov/gridpoints/{grid_id}/{grid_x},{grid_y}/forecast'
+    forecast_resp = requests.get(forecast_url)
+
+    if forecast_resp.status_code != 200:
+        return jsonify({"error": "Failed to fetch weather data"})
+    data = forecast_resp.json()
     
     periods = data["properties"]["periods"]
 
